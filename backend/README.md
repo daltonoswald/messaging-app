35fec53e639cdb829a00bcb5a3269ce6bf7de59b
daltonoswald <daltonoswald@gmail.com>
2024-07-03 14:06:42 -0400

    initial commit and user model added

---
+++ backend/README.md
@@ -0,0 +1,5 @@
+# messaging-app
+The Odin Project Messaging App Backend
+
+Mostly everything has been setup or installed. 
+Still need to create the MongoDB database.
---
+++ backend/models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    first_name: { type: String, required: true, maxLength: 50 },
+    last_name: { type: String, required: true, maxLength: 50 },
+    username: { type: String, required: true, maxLength: 50 },
+    password: { type: String, required: true, minLength: 8 },
+    profile_picture: { type: String, required: false },
+    bio: {type: String, required: true, maxLength: 100, default: "Hi, I just joined!" },
+    date_joined: { type: String, default: Date.now() },
+    friends: [
+        { type: Schema.Types.ObjectId, ref: "User", required: false }
+    ],
+    chats: [
+        {type: Schema.Types.ObjectId, ref: "Chats", required: false }
+    ]
+})
