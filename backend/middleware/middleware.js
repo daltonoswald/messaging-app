6b13f81a8b103b6babe9b2bbdd1faacf50016109
daltonoswald <daltonoswald@gmail.com>
2024-08-07 12:26:45 -0400

    added railway variable to middlware

--- backend/middleware/middleware.js
+++ backend/middleware/middleware.js
@@ -1,14 +1,13 @@
 const jwt = require('jsonwebtoken');
 
 const opts = { expiresIn: '1d' };
-const secret = process.env.TOKEN_KEY;
+const secret = process.env.TOKEN_KEY || TOKEN_KEY;
 const generateToken = (user) => jwt.sign({ id: user._id, username: user.username }, secret, opts);
 
 const verifyToken = (token) => jwt.verify(token, secret);
 
 const alreadyLoggedIn = (req, res, next) => {
     const alreadyLoggedIn = req.get('authenticationToken');
-    console.log(`already logged in = ` + alreadyLoggedIn);
     
     if (alreadyLoggedIn) {
         res.sendStatus(400);
